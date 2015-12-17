474a15ff879dae59432b8f230cc6258cf924a01a
ch710798472 <710798472@qq.com>
2015-12-17 21:00:25 +0800

    update

--- start/brain.py
+++ start/brain.py
@@ -3,14 +3,14 @@ import os
 
 kernel = aiml.Kernel()
 
-if os.path.isfile("bot_brain.brn"):
-    kernel.bootstrap(brainFile = "bot_brain.brn")
+if os.path.isfile("rebot_brain.brn"):
+    kernel.bootstrap(brainFile = "rebot_brain.brn")
 else:
-    kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
-    kernel.saveBrain("bot_brain.brn")
+    kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml pattern")
+    kernel.saveBrain("rebot_brain.brn")
 # kernel = aiml.Kernel()
 # kernel.learn("std-startup.xml")
-# kernel.respond("load aiml b")
+# kernel.respond("load aiml pattern")
 
 print "hello, you can talk to me! Just tell me ~"
 while True:
@@ -18,7 +18,7 @@ while True:
     if message == "quit":
         exit()
     elif message == "save":
-        kernel.saveBrain("bot_brain.brn")
+        kernel.saveBrain("rebot_brain.brn")
     else:
         bot_response = kernel.respond(message)
         print "robot say: %s" % bot_response
